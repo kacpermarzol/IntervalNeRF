f5ced0ddcb94a5744c3f8414e9cd31d9d0be8d8e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-10-27 23:56:16 +0200

    logger fix

--- run_nerf.py
+++ run_nerf.py
@@ -295,11 +295,13 @@ def raw2outputs(raw, z_vals, rays_d, raw_noise_std=0, white_bkgd=False, pytest=F
 
     alpha = raw2alpha(raw[..., 3] + noise, dists)  # [N_rays, N_samples]
     # weights = alpha * tf.math.cumprod(1.-alpha + 1e-10, -1, exclusive=True)
-    weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)), 1. - alpha + 1e-10], -1), -1)[:, :-1]
+    t1 = torch.ones((alpha.shape[0], 1)).to(alpha.device)
+    weights = alpha * torch.cumprod(torch.cat([t1, 1. - alpha + 1e-10], -1), -1)[:, :-1]
     rgb_map = torch.sum(weights[..., None] * rgb, -2)  # [N_rays, 3]
 
     depth_map = torch.sum(weights * z_vals, -1)
-    disp_map = 1. / torch.max(1e-10 * torch.ones_like(depth_map), depth_map / torch.sum(weights, -1))
+    t1_2 = torch.ones_like(depth_map).to(alpha.device)
+    disp_map = 1. / torch.max(1e-10 * t1_2, depth_map / torch.sum(weights, -1))
     acc_map = torch.sum(weights, -1)
 
     if white_bkgd:
